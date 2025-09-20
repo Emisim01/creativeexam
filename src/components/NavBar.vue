23abfcea6b32d4b13a46eeaedbf2f4673fa9b8c8
Emisim01 <emisim01@easv365.dk>
2025-09-20 14:05:51 +0200

    navbar temp. fixed/lavet

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,41 @@
 <template>
-  <nav class="p-4 font-bold flex justify-center items-center gap-4">
+  <nav class="p-4 font-bold flex justify-center gap-4">
      <div class="hidden md:flex gap-4 ">
       <RouterLink to="/" class="nav-link">Home</RouterLink>
       <RouterLink to="/#recipes" class="nav-link">Recipes</RouterLink>
       <RouterLink to="/#generator" class="nav-link">Generator</RouterLink>
       <RouterLink to="/#login/sign up" class="nav-link">Login/sign up</RouterLink>
-    </div>
+        </div>
+       <!-- Mobil knap -->
+    <button class="md:hidden text-2xl" @click="toggleMenu">☰</button>
   </nav>
-  <div>
-
+  <!-- Mobil menu dropdown -->
+  <div v-if="showMenu" class="hamburgerMobile flex flex-col gap-2 p-4 md:hidden bg-purple-300 text-white items-center" @mouseleave="closeMenu">
+    <RouterLink to="/" class="nav-link">Home</RouterLink>
+    <RouterLink to="/#recipes" class="nav-link">Recipes</RouterLink>
+    <RouterLink to="/#generator" class="nav-link">Generator</RouterLink>
+    <RouterLink to="/#login/sign up" class="nav-link">Login/sign up</RouterLink>
   </div>
+
 </template>
 
 <script setup>
+  import { RouterLink } from 'vue-router'
+  import { ref } from 'vue'
 
+const showMenu = ref(false)
+
+const toggleMenu = () => {
+  showMenu.value = !showMenu.value
+    console.log('toggleMenu', showMenu.value)
+}
+
+const closeMenu = () => {
+  showMenu.value = false
+}
 </script>
 
-<style scoped>
+<style>
 nav {
   font-size: 21px;
   align-items: center;
@@ -27,5 +46,16 @@ nav {
 
 .nav-link {
   color: white;
+}
+
+button {
+  color: white;
+  font-size: 40px;
+  cursor: pointer;
+}
+
+.hamburgerMobile {
+  font-size: 25px;
+  /* font-family: 'MerriweatherSans'; */
 }
 </style>
