8a71322736e9861f85953f1622b25a96bf8e4132
Emisim01 <emisim01@easv365.dk>
2025-09-22 16:51:12 +0200

    små små ændringer + hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
+ <img
+      src="@/assets/heroImg.png" alt="Hero" class="w-screen h-[80vw] min-h-[250px] max-h-[500px] object-cover block"
+    />
+  </div>
+  <div>
 
+      <p class="p-4 text-4xl font-bold text-white flex justify-center gap-4">Find your next creative recipe!</p>
   </div>
 </template>
 
